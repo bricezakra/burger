f60f9919a2263cb1adfb4c72731296f4a0d9f0dc
bricezakra <bricezakra@live.com>
2019-10-21 12:50:07 -0500

    burgers_controller.js file started

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,8 @@
+// initializes Node.js package
+var express = require("express");
+
+// sets up routing
+var router = express.Router();
+
+// imports burger model
+var burger = require("../models/burger.js");
