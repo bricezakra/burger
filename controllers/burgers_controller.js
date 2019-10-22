227bd1f3872792e81f63b523e49892c099626d1b
bricezakra <bricezakra@live.com>
2019-10-21 19:44:37 -0500

    burgers_controller.js updated

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -9,20 +9,20 @@ var burger = require("../models/burger.js");
 
 // default route to display all burgers
 router.get("/", function(request, response) {
-    burger.select(function(data) {
+    burger.selectAll(function(data) {
         response.render("index", { burgers: data });
     });
 });
 
 // route to add a new burger to the burger database
-router.post("/api/burgers", function(request, response) {
+router.post("/burgers", function(request, response) {
     burger.insert(request.body.burgerName, function(data) {
         response.json({ id: data.insertId });
     });
 });
 
 // route to update the status of a burger in the database
-router.put("/api/burgers/:id", function(request, response) {
+router.put("/burgers/:id", function(request, response) {
     burger.update(request.body.devoured, request.params.id, function(data) {
         if (data.affectedRows === 0) {
             return response.status(404).end();
